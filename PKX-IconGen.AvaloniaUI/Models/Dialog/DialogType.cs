477293eca82e74e2cb83addf874f0093a549a7a7
Samuel Caron <samuel.caron101@gmail.com>
2021-12-29 12:37:28 -0500

    License in source

--- PKX-IconGen.AvaloniaUI/Models/Dialog/DialogType.cs
+++ PKX-IconGen.AvaloniaUI/Models/Dialog/DialogType.cs
@@ -1,4 +1,23 @@
-﻿namespace PKXIconGen.AvaloniaUI.Models.Dialog
+﻿#region License
+/*  PKX-IconGen.AvaloniaUI - Avalonia user interface for PKX-IconGen.Core
+    Copyright (C) 2021-2022 mikeyX#4697
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+*/
+#endregion
+
+namespace PKXIconGen.AvaloniaUI.Models.Dialog
 {
     public enum DialogType
     {
--- PKX-IconGen.AvaloniaUI/Utils.cs
+++ PKX-IconGen.AvaloniaUI/Utils.cs
@@ -1,4 +1,23 @@
-﻿using Avalonia;
+﻿#region License
+/*  PKX-IconGen.AvaloniaUI - Avalonia user interface for PKX-IconGen.Core
+    Copyright (C) 2021-2022 mikeyX#4697
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+*/
+#endregion
+
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using PKXIconGen.AvaloniaUI.Models.Dialog;
