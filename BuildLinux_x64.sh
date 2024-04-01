54b9b4be661dea82447f621c26cce620e62cce91
Samuel Caron <samuel.caron101@gmail.com>
2024-04-01 12:13:28 -0400

    Clear previous publish for manual publishes

--- BuildLinux_x64.sh
+++ BuildLinux_x64.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+rm -r ./Publish/linux-x64
+
 dotnet publish ./PKX-IconGen.AvaloniaUI/PKX-IconGen.AvaloniaUI.csproj --configuration Release --framework net8.0 --self-contained true --arch x64 --os linux -p:PublishSingleFile=true -p:EnableCompressionInSingleFile=true --output ./Publish/linux-x64
--- BuildWindows_x64.bat
+++ BuildWindows_x64.bat
@@ -1,1 +1,3 @@
+rmdir /Q /S ./Publish/win-x64
+
 dotnet publish ./PKX-IconGen.AvaloniaUI/PKX-IconGen.AvaloniaUI.csproj --configuration Release --framework net8.0 --self-contained true --arch x64 --os win -p:PublishSingleFile=true -p:EnableCompressionInSingleFile=true --output ./Publish/win-x64
