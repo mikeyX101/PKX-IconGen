2415a0a167fdf0c4e1bb94af93b4481b1a0b4f08
Samuel Caron <samuel.caron101@gmail.com>
2024-05-09 19:32:19 -0400

    More windows build fixes

--- BuildWindows_x64.bat
+++ BuildWindows_x64.bat
@@ -1,6 +1,6 @@
-rmdir /Q /S ./Publish/win-x64
+rmdir /Q /S ".\Publish\win-x64"
 
 dotnet publish ./PKX-IconGen.AvaloniaUI/PKX-IconGen.AvaloniaUI.csproj --configuration Release --framework net8.0 --self-contained true --arch x64 --os win -p:PublishSingleFile=true -p:EnableCompressionInSingleFile=true --output ./Publish/win-x64
 
 ren ".\Publish\win-x64\PKX-IconGen.AvaloniaUI.pdb" "PKX-IconGen.pdb"
-ren ".\Publish\win-x64\PKX-IconGen.AvaloniaUI" "PKX-IconGen"
+ren ".\Publish\win-x64\PKX-IconGen.AvaloniaUI.exe" "PKX-IconGen.exe"
